9761e5ff917c51b6729f30963503d16c258d49de
Lukas Staub <main@lukasstaub.dev>
2021-12-25 10:49:26 +0100

    Initial Hydration

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,90 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
+import { Links, LinksFunction, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
 import type { MetaFunction } from "remix";
 
+import contentStylesUrl from "./css/content-styles.css";
+import stylesUrl from "./css/index.css";
+
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+    return { title: "New Remix App" };
+};
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: stylesUrl,
+        },
+        {
+            rel: "stylesheet",
+            href: contentStylesUrl,
+        },
+    ];
 };
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossOrigin="anonymous" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/brands.min.css" integrity="sha512-apX8rFN/KxJW8rniQbkvzrshQ3KvyEH+4szT3Sno5svdr6E/CP0QE862yEeLBMUnCqLko8QaugGkzvWS7uNfFQ==" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="apple-touch-icon" href="/assets/logo192.png" />
+                <link rel="icon" href="/assets/favicon.ico" />
+
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <header>
+                    <a href="/" className="page-title">
+                        lukasstaub.dev
+                    </a>
+
+                    <section className="mobile-hidden">
+                        <a href="/about">i18n.nav_about</a>
+                        <a href="/contact">i18n.nav_contact</a>
+                        <a href="/projects">i18n.nav_projects</a>
+                        <a href="/links">i18n.nav_links</a>
+                    </section>
+
+                    <i id="show-menu" className="fas fa-bars desktop-hidden hamburger-btn"></i>
+                    <i id="close-menu" className="fas fa-times hamburger-btn hidden"></i>
+                </header>
+                <div className="content">
+                    <Outlet />
+                </div>
+                <footer>
+                    <p>Copyright &copy;{new Date().getFullYear()} Lukas Staub.</p>
+                    <section>
+                        <a href="https://admin.lukasstaub.dev" target="_blank">
+                            i18n.footer_login
+                        </a>
+                        <a href="/about-page">i18n.footer_page_resources</a>
+                    </section>
+                </footer>
+
+                <div className="mobile-menu" id="mobile-menu">
+                    <a className="btn" href="/about">
+                        i18n.nav_about
+                    </a>
+                    <a className="btn" href="/contact">
+                        i18n.nav_contact
+                    </a>
+                    <a className="btn" href="/projects">
+                        i18n.nav_projects
+                    </a>
+                    <a className="btn" href="/links">
+                        i18n.nav_links
+                    </a>
+                </div>
+                <ScrollRestoration />
+                <Scripts />
+                {process.env.NODE_ENV === "development" && <LiveReload />}
+            </body>
+        </html>
+    );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,71 @@
+import { useEffect } from "react";
+import { LoaderFunction, useLoaderData } from "remix";
+import client from "~/config/client";
+
+export const loader: LoaderFunction = async () => {
+    const projects = await client.projects.findMany();
+
+    return projects;
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    const projects = useLoaderData();
+
+    useEffect(() => {
+        console.log(projects);
+    }, []);
+
+    return (
+        <main className="home-content">
+            <div className="home-inner-top">
+                <section className="message">
+                    <p className="top">i18n.home_top</p>
+                    <p className="big">i18n.home_middle</p>
+                    <p className="bottom">
+                        i18n.home_bottom_one
+                        <br />
+                        i18n.home_bottom_two
+                    </p>
+                </section>
+                <section>
+                    <img src="/assets/front-asset.svg" alt="hero-asset" />
+                </section>
+                <section>
+                    <a href="#bottom">
+                        <i className="fas fa-chevron-down"></i>
+                    </a>
+                </section>
+            </div>
+
+            <section id="bottom" className="card-container">
+                <div>
+                    <h1>i18n.home_latest_projects</h1>
+                    <a href="/projects" className="btn-link">
+                        i18n.generic_view_all
+                    </a>
+                </div>
+                {projects.length > 0 ? (
+                    <div>
+                        {projects.map((project: any) => {
+                            return (
+                                <div key={project.slug} className="project-card">
+                                    <img src={project.image_url} alt="project-logo" />
+                                    <div>
+                                        <h1>{project.name}</h1>
+                                        <a href="/projects/<%= project.slug %>" className="btn-link">
+                                            i18n.generic_read_more
+                                        </a>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div>
+                        <p>i18n.generic_nothing_to_see</p>
+                    </div>
+                )}
+            </section>
+        </main>
+    );
 }
