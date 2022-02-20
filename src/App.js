6d6986db7343464cf4c71db8059f69595026f4cc
Lukas Staub <main@lukasstaub.dev>
2022-02-20 09:51:12 +0100

    Fixed img issue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import state from "./pic.svg";
 function App() {
   return (
     <div className="app__container">
-      <img src={state} />
+      <img src={state} alt="Construction State" />
       <h1>This Site is currently being restructured.</h1>
       <p>ETA: Unknown</p>
     </div>
